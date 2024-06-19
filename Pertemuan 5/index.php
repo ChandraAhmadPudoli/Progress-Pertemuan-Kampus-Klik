c2eb533a63078909ee05f0df230313404dd93f5d
ChandraAhmadPudoli <2210631170114@student.unsika.ac.id>
2024-06-19 11:25:32 +0700

    Upload Progress Kampus Klik

---
+++ Pertemuan 5/index.php
@@ -0,0 +1,21 @@
+<?php
+// Membuat variabel
+$nama_mahasiswa = "Ariel Tatum";
+$nama_kamu = "Andi";
+
+echo "Hallo " . $nama_mahasiswa . " Selamat datang, saya " . $nama_kamu;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 5</title>
+</head>
+<body>
+    <br>
+    coba
+    Calon Pacar Saya
+</body>
+</html>
